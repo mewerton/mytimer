0d80a9cd7192041d6be2db43f27371e66212753d
Mewerton Melo <mewerton@gmail.com>
2022-11-08 15:04:54 -0300

    Add files via upload

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,4 @@
+import { Placeholder } from "phosphor-react";
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
@@ -26,11 +27,45 @@ export const FormContainer = styled.div`
     font-size: 1.125rem;
     font-weight: bold;
     flex-wrap: wrap;
+`
 
+const BaseInput = styled.input`
+    background: transparent;
+    height: 2.5rem;
+    border: 0;
+    border-bottom: 2px solid ${props => props.theme['gray-500']};
+    font-weight: bold;
+    font-size: 1.125rem;
+    padding: 0 0.5rem;
+    color: ${props => props.theme['gray-100']};
+
+    &:focus{
+        box-shadow: none;
+        border-color: ${props => props.theme['green-500']};
+    }
+
+    &::placeholder{
+    color: ${props => props.theme['gray-500']};
+    }
 
 `
 
+
+
+export const TaskInput = styled(BaseInput)`
+    flex: 1;
+
+    &::-webkit-calendar-picker-indicator{
+        display: none !important;
+    }
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+    width: 4rem;
+`
+
 export const CountdownContainer = styled.div`
+
     font-family: 'Roboto Mono', monospace;
     font-size: 10rem;
     line-height: 8rem;
@@ -55,4 +90,31 @@ export const Separator = styled.div`
     overflow: hidden;
     display: flex;
     justify-content: center;
+`
+export const StartCountdownButton = styled.button`
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    background: ${props => props.theme['green-500']};
+    color: ${props => props.theme['gray-100']};
+
+    &:disabled{
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover{
+        background: ${props => props.theme['green-700']};
+    }
 `
